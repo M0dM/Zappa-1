kikeh/Zappa

Tests for SQS as async service

tests/tests_async.py
@@ -1,4 +1,6 @@
 # -*- coding: utf8 -*-
+import json
+
 import boto3
 import mock
 import os
@@ -9,7 +11,7 @@ try:
 except ImportError:
     from unittest.mock import patch
 
-from zappa.async import AsyncException, LambdaAsyncResponse, SnsAsyncResponse
+from zappa.async import AsyncException, LambdaAsyncResponse, SnsAsyncResponse, SqsAsyncResponse
 from zappa.async import import_and_get_task, \
                         get_func_task_path
 
@@ -48,6 +50,7 @@ class TestZappa(unittest.TestCase):
         l = LambdaAsyncResponse(boto_session=boto_session)
         # s = SnsAsyncResponse()
         s = SnsAsyncResponse(arn="arn:abc:def", boto_session=boto_session)
+        q = SqsAsyncResponse(queue_url="https://example.com", boto_session=boto_session)
 
     def test_nofails_funcs(self):
         funk = import_and_get_task("tests.test_app.async_me")
@@ -85,9 +88,57 @@ class TestZappa(unittest.TestCase):
                                  "Running async!")
 
         # And check the dispatching class got called correctly
-        lambda_async_mock.assert_called_once()
-        lambda_async_mock.assert_called_with(aws_region='us-east-1',
+        lambda_async_mock.assert_called_once_with(aws_region='us-east-1',
                                              capture_response=False,
+                                             delay_seconds=0,
                                              lambda_function_name="MyLambda")
         lambda_async_mock.return_value.send.assert_called_with(
             get_func_task_path(async_me), ("qux",), {})
+
+
+    def test_async_sqs_call(self):
+        """
+        Call a task with sqs async service.
+        """
+
+        async_sqs_me = import_and_get_task("tests.test_app.async_sqs_me")
+        sqs_client_mock = mock.Mock()
+        sqs_client_mock.get_queue_url = mock.MagicMock(
+            return_value={'QueueUrl': 'https://us-east-1.queue.amazonaws.com/1'}
+        )
+        sqs_client_mock.send_message = mock.MagicMock(
+            return_value={
+                'MD5OfMessageBody': 'string',
+                'MD5OfMessageAttributes': 'string',
+                'MessageId': '1234',
+                'SequenceNumber': '1'
+            }
+        )
+        with mock.patch('zappa.async.SQS_CLIENT', sqs_client_mock):
+            # First check that it still runs synchronously by default
+            self.assertEqual(async_sqs_me("123"),
+                             "run async with sqs service when on lambda 123")
+
+            # Now patch the environment to make it look like we are running on
+            # AWS Lambda
+            options = {
+                'AWS_LAMBDA_FUNCTION_NAME': 'MyLambda',
+                'AWS_REGION': 'us-east-1'
+            }
+            with mock.patch.dict(os.environ, options):
+                async_sqs_me("qux")
+
+        # And check the sqs client got invoked correctly
+        sqs_client_mock.get_queue_url.assert_called_once_with(QueueName='MyLambda-zappa-async')
+        sqs_client_mock.send_message.assert_called_once_with(
+            QueueUrl='https://us-east-1.queue.amazonaws.com/1',
+            MessageBody=json.dumps({
+                "task_path": get_func_task_path(async_sqs_me),
+                "capture_response": False,
+                "response_id": None,
+                "args": ["qux"],
+                "kwargs": {},
+                "zappaAsyncCommand": "zappa.async.route_sqs_task"
+            }),
+            DelaySeconds=0
+        )

tests/tests_placebo.py
@@ -399,6 +399,30 @@ class TestZappa(unittest.TestCase):
             }
         self.assertEqual("AWS SQS EVENT", lh.handler(event, None))
 
+        # Test AWS SQS async event
+        event = {
+            u'Records': [
+                {
+                    u'messageId': u'9f884e48-9445-48e8-9eb4-62e86ee69033',
+                    u'receiptHandle': u'AQEBJQ+/u6NsnT5t8Q/VbVxgdUl4TMKZ5FqhksRdIQvLBhwNvADoBxYSOVeCBXdnS9P+erlTtwEALHsnBXynkfPLH3BOUqmgzP25U8kl8eHzq6RAlzrSOfTO8ox9dcp6GLmW33YjO3zkq5VRYyQlJgLCiAZUpY2D4UQcE5D1Vm8RoKfbE+xtVaOctYeINjaQJ1u3mWx9T7tork3uAlOe1uyFjCWU5aPX/1OHhWCGi2EPPZj6vchNqDOJC/Y2k1gkivqCjz1CZl6FlZ7UVPOx3AMoszPuOYZ+Nuqpx2uCE2MHTtMHD8PVjlsWirt56oUr6JPp9aRGo6bitPIOmi4dX0FmuMKD6u/JnuZCp+AXtJVTmSHS8IXt/twsKU7A+fiMK01NtD5msNgVPoe9JbFtlGwvTQ==',
+                    u'body': u'{"task_path": "test_settings.aws_async_sqs_event", "capture_response": false, "response_id": null, "args": ["arg1", "arg2"], "kwargs": {"arg3": "varg3"}, "zappaAsyncCommand": "zappa.async.route_sqs_task"}',
+                    u'attributes': {
+                        u'ApproximateReceiveCount': u'1',
+                        u'SentTimestamp': u'1539243935446',
+                        u'SenderId': u'AROAIPI3A42BPBEHOTBFA',
+                        u'ApproximateFirstReceiveTimestamp': u'1539243935452'
+                    },
+                    u'messageAttributes': {},
+                    u'md5OfBody': u'7412d5bfaa2241ec53d5415535f4ee14',
+                    u'eventSource': u'aws:sqs',
+                    u'eventSourceARN': u'arn:aws:sqs:1',
+                    u'awsRegion': u'us-east-1'
+                }
+            ]
+        }
+        self.assertEqual("AWS ASYNC SQS EVENT", lh.handler(event, None))
+
+
         # Test Authorizer event
         event = {u'authorizationToken': u'hubtoken1', u'methodArn': u'arn:aws:execute-api:us-west-2:1234:xxxxx/dev/GET/v1/endpoint/param', u'type': u'TOKEN'}
         self.assertEqual("AUTHORIZER_EVENT", lh.handler(event, None))
@@ -455,6 +479,14 @@ class TestZappa(unittest.TestCase):
         zappa_cli.lambda_name = 'baby-flask-devor'
         zappa_cli.zappa.credentials_arn = 'arn:aws:iam::12345:role/ZappaLambdaExecution'
         resp = zappa_cli.status()
+
+    @placebo_session
+    def test_cli_schedule_async_resources(self, session):
+        zappa_cli = ZappaCLI()
+        zappa_cli.api_stage = 'schedule_async_resources_sqs'
+        zappa_cli.load_settings('test_settings.json', session)
+        zappa_cli.schedule()
+        zappa_cli.unschedule()
 
     ##
     # Let's Encrypt / ACME

M0dM/Zappa-1

tests/tests_async.py
@@ -1,4 +1,6 @@
 # -*- coding: utf8 -*-
+import json
+
 import boto3
 import mock
 import os
@@ -9,7 +11,7 @@ try:
 except ImportError:
     from unittest.mock import patch
 
-from zappa.asynchronous import AsyncException, LambdaAsyncResponse, SnsAsyncResponse
+from zappa.asynchronous import AsyncException, LambdaAsyncResponse, SnsAsyncResponse, SqsAsyncResponse
 from zappa.asynchronous import import_and_get_task, get_func_task_path
 
 
@@ -47,6 +49,7 @@ class TestZappa(unittest.TestCase):
         l = LambdaAsyncResponse(boto_session=boto_session)
         # s = SnsAsyncResponse()
         s = SnsAsyncResponse(arn="arn:abc:def", boto_session=boto_session)
+        q = SqsAsyncResponse(queue_url="https://example.com", boto_session=boto_session)
 
     def test_nofails_funcs(self):
         funk = import_and_get_task("tests.test_app.async_me")
@@ -84,9 +87,57 @@ class TestZappa(unittest.TestCase):
                                  "Running async!")
 
         # And check the dispatching class got called correctly
-        lambda_async_mock.assert_called_once()
-        lambda_async_mock.assert_called_with(aws_region='us-east-1',
+        lambda_async_mock.assert_called_once_with(aws_region='us-east-1',
                                              capture_response=False,
+                                             delay_seconds=0,
                                              lambda_function_name="MyLambda")
         lambda_async_mock.return_value.send.assert_called_with(
             get_func_task_path(async_me), ("qux",), {})
+
+
+    def test_async_sqs_call(self):
+        """
+        Call a task with sqs async service.
+        """
+
+        async_sqs_me = import_and_get_task("tests.test_app.async_sqs_me")
+        sqs_client_mock = mock.Mock()
+        sqs_client_mock.get_queue_url = mock.MagicMock(
+            return_value={'QueueUrl': 'https://us-east-1.queue.amazonaws.com/1'}
+        )
+        sqs_client_mock.send_message = mock.MagicMock(
+            return_value={
+                'MD5OfMessageBody': 'string',
+                'MD5OfMessageAttributes': 'string',
+                'MessageId': '1234',
+                'SequenceNumber': '1'
+            }
+        )
+        with mock.patch('zappa.asynchronous.SQS_CLIENT', sqs_client_mock, create=True):
+            # First check that it still runs synchronously by default
+            self.assertEqual(async_sqs_me("123"),
+                             "run async with sqs service when on lambda 123")
+
+            # Now patch the environment to make it look like we are running on
+            # AWS Lambda
+            options = {
+                'AWS_LAMBDA_FUNCTION_NAME': 'MyLambda',
+                'AWS_REGION': 'us-east-1'
+            }
+            with mock.patch.dict(os.environ, options):
+                async_sqs_me("qux")
+
+        # And check the sqs client got invoked correctly
+        sqs_client_mock.get_queue_url.assert_called_once_with(QueueName='MyLambda-zappa-async')
+        sqs_client_mock.send_message.assert_called_once_with(
+            QueueUrl='https://us-east-1.queue.amazonaws.com/1',
+            MessageBody=json.dumps({
+                "task_path": get_func_task_path(async_sqs_me),
+                "capture_response": False,
+                "response_id": None,
+                "args": ["qux"],
+                "kwargs": {},
+                "zappaAsyncCommand": "zappa.asynchronous.route_sqs_task"
+            }),
+            DelaySeconds=0
+        )

tests/tests_placebo.py
@@ -399,6 +399,30 @@ class TestZappa(unittest.TestCase):
             }
         self.assertEqual("AWS SQS EVENT", lh.handler(event, None))
 
+        # Test AWS SQS async event
+        event = {
+            u'Records': [
+                {
+                    u'messageId': u'9f884e48-9445-48e8-9eb4-62e86ee69033',
+                    u'receiptHandle': u'AQEBJQ+/u6NsnT5t8Q/VbVxgdUl4TMKZ5FqhksRdIQvLBhwNvADoBxYSOVeCBXdnS9P+erlTtwEALHsnBXynkfPLH3BOUqmgzP25U8kl8eHzq6RAlzrSOfTO8ox9dcp6GLmW33YjO3zkq5VRYyQlJgLCiAZUpY2D4UQcE5D1Vm8RoKfbE+xtVaOctYeINjaQJ1u3mWx9T7tork3uAlOe1uyFjCWU5aPX/1OHhWCGi2EPPZj6vchNqDOJC/Y2k1gkivqCjz1CZl6FlZ7UVPOx3AMoszPuOYZ+Nuqpx2uCE2MHTtMHD8PVjlsWirt56oUr6JPp9aRGo6bitPIOmi4dX0FmuMKD6u/JnuZCp+AXtJVTmSHS8IXt/twsKU7A+fiMK01NtD5msNgVPoe9JbFtlGwvTQ==',
+                    u'body': u'{"task_path": "test_settings.aws_async_sqs_event", "capture_response": false, "response_id": null, "args": ["arg1", "arg2"], "kwargs": {"arg3": "varg3"}, "zappaAsyncCommand": "zappa.asynchronous.route_sqs_task"}',
+                    u'attributes': {
+                        u'ApproximateReceiveCount': u'1',
+                        u'SentTimestamp': u'1539243935446',
+                        u'SenderId': u'AROAIPI3A42BPBEHOTBFA',
+                        u'ApproximateFirstReceiveTimestamp': u'1539243935452'
+                    },
+                    u'messageAttributes': {},
+                    u'md5OfBody': u'7412d5bfaa2241ec53d5415535f4ee14',
+                    u'eventSource': u'aws:sqs',
+                    u'eventSourceARN': u'arn:aws:sqs:1',
+                    u'awsRegion': u'us-east-1'
+                }
+            ]
+        }
+        self.assertEqual("AWS ASYNC SQS EVENT", lh.handler(event, None))
+
+
         # Test Authorizer event
         event = {'authorizationToken': 'hubtoken1', 'methodArn': 'arn:aws:execute-api:us-west-2:1234:xxxxx/dev/GET/v1/endpoint/param', 'type': 'TOKEN'}
         self.assertEqual("AUTHORIZER_EVENT", lh.handler(event, None))
@@ -455,6 +479,14 @@ class TestZappa(unittest.TestCase):
         zappa_cli.lambda_name = 'baby-flask-devor'
         zappa_cli.zappa.credentials_arn = 'arn:aws:iam::12345:role/ZappaLambdaExecution'
         resp = zappa_cli.status()
+
+    @placebo_session
+    def test_cli_schedule_async_resources(self, session):
+        zappa_cli = ZappaCLI()
+        zappa_cli.api_stage = 'schedule_async_resources_sqs'
+        zappa_cli.load_settings('test_settings.json', session)
+        zappa_cli.schedule()
+        zappa_cli.unschedule()
 
     ##
     # Let's Encrypt / ACME